414fa4a12dca32886bb2809c6b8575225cc02fb7
arupsaha07 <ARUP07@GMAIL.COM>
2025-04-20 03:10:51 +0530

    sidebar added

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 function Sidebar() {
+
+    const location = useLocation();
+    const isReactExample = location.pathname.startsWith('/example')
     return (
         <div className='w-64 h-screen bg-gray-100 text-white p-4 fixed left-0 top-0 overflow-y-auto flex flex-col'>
-            <NavLink to="/example/use-ref" className={({ isActive }) => `px-4 py-2 ${isActive ? 'text-orange-400' : 'text-gray-700'} hover:text-red-500`}>useRef hook</NavLink>
-            <NavLink to="/example/use-memo" className={({ isActive }) => `px-4 py-2 ${isActive ? 'text-orange-400' : 'text-gray-700'} hover:text-red-500`}>useMemo hook</NavLink>
-            <NavLink to="/example/use-callback" className={({ isActive }) => `px-4 py-2 ${isActive ? 'text-orange-400' : 'text-gray-700'} hover:text-red-500`}>useCallback hook</NavLink>
+            {
+                isReactExample &&
+                <>
+                    <NavLink to="/example/use-context" className={({ isActive }) => `px-4 py-2 ${isActive ? 'text-orange-400' : 'text-gray-700'} hover:text-red-500`}>useContext hook</NavLink>
+                    <NavLink to="/example/use-memo" className={({ isActive }) => `px-4 py-2 ${isActive ? 'text-orange-400' : 'text-gray-700'} hover:text-red-500`}>useMemo hook</NavLink>
+                    <NavLink to="/example/use-callback" className={({ isActive }) => `px-4 py-2 ${isActive ? 'text-orange-400' : 'text-gray-700'} hover:text-red-500`}>useCallback hook</NavLink>
+                </>
+            }
         </div>
     )
 }
