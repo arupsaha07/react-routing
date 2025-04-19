9d5d69f5532b21ad997cdef3b7c2bd936661fd9f
arupsaha07 <ARUP07@GMAIL.COM>
2025-04-20 03:14:32 +0530

    home added

---
+++ src/components/examples/ExampleHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ExampleHome() {
+    return (
+        <div className="p-4">
+            <h2 className="text-xl font-bold">React Hook Examples</h2>
+            <p>Select a hook from the sidebar to explore more.</p>
+        </div>
+    )
+}
+
+export default ExampleHome
